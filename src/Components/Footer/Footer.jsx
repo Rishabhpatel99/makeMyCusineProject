808dfc423a5dc4ca06c065aaa2719885e94b9daf
rishabhmpatel99@gmail.com <rishabhmpatel99@gmail.com>
2022-09-05 21:51:23 -0400

    changed footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,39 +1,49 @@
-import React from 'react'
+import React from "react";
 
-
-function Footer(){
-return(
+function Footer() {
+  return (
     <footer className="dark-footer skin-dark-footer">
-
-    
-    <div className="footer-bottom">
+      <div className="footer-bottom">
         <div className="container">
-            <div className="row align-items-center">
-                
-                <div className="col-lg-6 col-md-6">
-                    <p className="mb-0">© 2021 Make My Recipe Designd By Harsh | Rahul | Happy  All Rights Reserved</p>
-                </div>
-                
-                <div className="col-lg-6 col-md-6 text-right">
-                    <ul className="footer-bottom-social">
-                        <li><a href="freelancing.html#"><i className="ti-facebook"></i></a></li>
-                        <li><a href="freelancing.html#"><i className="ti-twitter"></i></a></li>
-                        <li><a href="freelancing.html#"><i className="ti-instagram"></i></a></li>
-                        <li><a href="freelancing.html#"><i className="ti-linkedin"></i></a></li>
-                    </ul>
-                </div>
-                
+          <div className="row align-items-center">
+            <div className="col-lg-6 col-md-6">
+              <p className="mb-0">
+                © 2021 Make My Recipe Designd By Rishabh All Rights Reserved
+              </p>
             </div>
+
+            <div className="col-lg-6 col-md-6 text-right">
+              <ul className="footer-bottom-social">
+                <li>
+                  <a href="freelancing.html#">
+                    <i className="ti-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="freelancing.html#">
+                    <i className="ti-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="freelancing.html#">
+                    <i className="ti-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/rishabhpatel99/"
+                    target="blank"
+                  >
+                    <i className="ti-linkedin"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-    </div>
-</footer>
-
-
-
-
-
-);
-
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
